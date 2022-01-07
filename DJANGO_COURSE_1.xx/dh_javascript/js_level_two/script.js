bc93e3cd6cb86435be32e18d9b6ea16bca174fae
Doug Holmes <78378906+douglholmes@users.noreply.github.com>
2022-01-07 17:35:28 -0600

    part 2

---
+++ DJANGO_COURSE_1.xx/dh_javascript/js_level_two/script.js
@@ -0,0 +1,22 @@
+//part 1
+
+// function helloYou(name) {
+//     console.log("hello " + name);
+// }
+
+// function addNum(num1, num2) {
+//     console.log(num1+num2);
+// }
+
+// function helloSomeone(name="Frankie") {
+//     console.log("Hello " + name)
+// }
+
+// function formal(name="Sam", title="sir") {
+//     return title + " " + name
+// }
+
+// function timesFive(numInput) {
+//     var result = numInput*5
+//     return result
+// }
