c228ae50c9f9429acb881b4317f06301b7c20d69
Doug Holmes <78378906+douglholmes@users.noreply.github.com>
2022-01-19 13:21:13 -0600

    Finished Connect Four game

--- DJANGO_COURSE_1.xx/jQuery/jquery_project.js
+++ DJANGO_COURSE_1.xx/jQuery/jquery_project.js
@@ -7,32 +7,32 @@ var player2Color = 'rgb(237, 45, 73)'
 var game_on = true;
 var table = $('table tr');
 
-function reportWin(rowNum, colNum) {
+function reportWin(rowNum,colNum) {
     console.log("You won starting at this row,col");
     console.log(rowNum);
     console.log(colNum);
 }
 
-function changeColor(rowIndex, colIndex, color) {
+function changeColor(rowIndex,colIndex,color) {
     return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color);
 }
 
-function returnColor(rowIndex, colIndex) {
+function returnColor(rowIndex,colIndex) {
     return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
 
 function checkBottom(colIndex) {
-    var colorReport = returnColor(5, colIndex);
+    var colorReport = returnColor(5,colIndex);
     for (var row = 5; row > -1; row--){
-        colorReport = returnColor(row, colIndex);
-        if (colorReport === 'rgb(128,128,128)') {
+        colorReport = returnColor(row,colIndex);
+        if (colorReport === 'rgb(128, 128, 128)') {
             return row
         }
     }
 }
 
 function colorMatchCheck(one,two,three,four) {
-    return (one === two && one === three && one === four && one !== 'rgb(128,128,128)' && one!== undefined)
+    return (one === two && one === three && one === four && one !== 'rgb(128, 128, 128)' && one!== undefined)
 }
 
 // Check for Horizontal Wins
@@ -83,3 +83,38 @@ function diagonalWinCheck() {
         }
     }
 }
+
+
+//START WITH PLAYER ONE
+var currentPlayer = 1;
+var currentName = player1;
+var currentColor = player1Color;
+
+$('h3').text(player1 + " it is your turn, pick a column to drop in!")
+
+$('.board button').on('click',function() {
+
+    var col = $(this).closest("td").index();
+
+    var bottomAvail = checkBottom(col);
+
+    changeColor(bottomAvail,col,currentColor);
+
+    if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
+        $('h1').text(currentName + " You have won!")
+        $('h3').fadeOut('fast');
+        $('h2').fadeOut('fast');
+    }
+
+    currentPlayer = currentPlayer * -1 ;
+
+    if (currentPlayer === 1) {
+        currentName = player1;
+        $('h3').text(currentName+": it is your turn.");
+        currentColor == player1Color;
+    }else {
+        currentName = player2;
+        $('h3').text(currentName+": it is your turn.");
+        currentColor = player2Color;
+    }
+})
