cf51673ef27c410e3431bcbcbb9a80a1b388a346
Doug Holmes <78378906+douglholmes@users.noreply.github.com>
2022-01-25 11:42:41 -0600

    python review

--- DJANGO_COURSE_1.xx/Python_Level_One/Part6_Exercise_Review.py
+++ DJANGO_COURSE_1.xx/Python_Level_One/Part6_Exercise_Review.py
@@ -72,4 +72,4 @@ name = "Sammy"
 # Use print formatting to print the following string:
 "Hello my dog's name is Sammy and he is 4 years old"
 
-print("Hello my dog's name is {name} and he is {age} years old".format(age = 4, name = "Sammy"))
+print("Hello my dog's name is {name} and he is {age} years old".format(age = age, name = name))
