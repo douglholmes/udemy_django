b36a5fa3349053c5abf436299796a4f635bf9435
Doug Holmes <78378906+douglholmes@users.noreply.github.com>
2022-02-04 17:19:04 -0600

    Faker random data populator

---
+++ DJANGO_COURSE_1.xx/django/first_project/populate_first_app.py
@@ -0,0 +1,44 @@
+import imp
+import os
+from re import T
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','first_project.settings')
+
+import django
+django.setup()
+
+##FAKE POPULATION SCRIPT
+
+import random
+from first_app.models import AccessRecord,Webpage,Topic
+from faker import Faker
+
+fakegen = Faker()
+topics = ['Search','Social','Marketplace','News','Games']
+
+def add_topic():
+    t = Topic.objects.get_or_create(topic_name = random.choice(topics))[0]
+    t.save()
+    return t
+
+def populate(N=5):
+
+    for entry in range(N):
+
+        #get topic for entry
+        top = add_topic()
+
+        #create fake data for that entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        #create new webpage entry
+        webpg = Webpage.objects.get_or_create(topic = top, url = fake_url, name = fake_name)[0]
+
+        #create fake access record for that webpage
+        acc_rec = AccessRecord.objects.get_or_create(name = webpg,date=fake_date)[0]
+
+if __name__ == '__main__':
+    print("populating script!")
+    populate(20)
+    print("populating complete!")
